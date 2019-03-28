299f67379a2ec81a1693b0813ac3dfd8ad4fec8b
Dale Sublett <djsublett04@gmail.com>
2019-03-28 00:46:32 +0000

    Automatically backed up by Learn

---
+++ loops.js
@@ -0,0 +1,37 @@
+
+
+function forLoop(array) {
+  for (i = 0; i < 25; i++){
+    if (i === 1){
+    array.push("I am 1 strange loop.");
+  } else {
+    array.push(`I am ${i} strange loops.`);
+  }
+  
+}
+return array
+}
+let n = 50;
+function whileLoop(n) {
+  while (n > 0) {
+    console.log(n--);
+  }
+  return 'done';
+}
+
+
+
+function doWhileLoop(num) {
+  var i = 0;
+  do {
+     console.log("I run once regardless."); 
+    
+  } while (incrementVariable(i) < num);
+}
+
+function incrementVariable(num) {
+  num = num + 1;
+  return num;
+}
+
+
