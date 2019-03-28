95a296736090cd5ca05384a210c3b3d288756bf2
Dale Sublett <djsublett04@gmail.com>
2019-03-28 00:47:16 +0000

    Automatically backed up by Learn

--- loops.js
+++ loops.js
@@ -24,6 +24,7 @@ function whileLoop(n) {
 function doWhileLoop(num) {
   var i = 0;
   do {
+     console.log(i);
      console.log("I run once regardless."); 
     
   } while (incrementVariable(i) < num);
